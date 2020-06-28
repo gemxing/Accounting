20b53ed882f980616917f6ff2c5dc8a0bc28ad66
potS <1820346204@qq.com>
2020-06-29 03:42:28 +0800

    在Types.vue使用js实现了一个组件

--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-                <ul class="types">
-                    <li class="selected">支出</li>
-                    <li>收入</li>
-                </ul>
-            </div>
+        <ul class="types">
+         <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+         <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+        </ul>
+    </div>
 </template>
 
-<script lang="ts">
+<script>
     import Vue from 'vue'
 
     export default Vue.extend({
-        name:'Types'
+        name:'Types',
+        data(){
+            return{
+                type: '-'//'-'表示支出，‘+’表示收入
+            }
+        },
+        methods:{
+            selectType(type){//type只能是+-其中一个
+                if(type !== '-'&& type!== '+'){
+                    throw new Error('type is unknown')
+                }
+                this.type = type
+            }
+        }
     })
 </script>
 
