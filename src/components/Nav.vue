474e8cf04387826dbd1c98aa80b527a6e0b77d97
potS <1820346204@qq.com>
2020-06-28 20:37:11 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,56 @@
 <template>
-    <div class="nav">
-      <router-link to="/money">记账</router-link>
+    <nav>
+      <router-link to="/money" class="item">
+      <Icon name="money"/>
+      记账
+      </router-link>
       |
-      <router-link to="/labels">标签</router-link>
+      <router-link to="/labels" class="item">
+      <Icon name="label"/>
+      标签
+      </router-link>
       |
-      <router-link to="/statistics">数据统计</router-link>
-    </div>
+      <router-link to="/statistics" class="item">
+      <Icon name="statistics"/>
+      统计
+      </router-link>
+    </nav>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
+    // import router from '../router/index';
+    // import content from '../shims-vue';
 
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+    try{
+        importAll(require.context('../assets/icons',true,/\.svg$/));
+    }catch(error){
+        console.log(error);
+    }
+    
     export default Vue.extend({
         name: 'Nav'
     })
 </script>
 
 <style scoped>
-    .nav{
-        border: 1px solid red;
+    nav{
+        display: flex;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+        flex-direction: row;
+        font-size: 12px;
+    }
+    nav > .item{
+            padding: 2px 0;
+            width:33.333333%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    nav .icon{
+        width:32px;
+        height: 32px;
     }
 </style>
