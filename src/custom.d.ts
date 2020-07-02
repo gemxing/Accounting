3c7a106234dee1fee95d0add69d160e56a56a993
potS <1820346204@qq.com>
2020-07-02 17:17:44 +0800

    使用全局tagList

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -6,3 +6,20 @@ type RecordItem = {
     //添加created字段
     createdAt?: Date;  // 类 / 构造函数，这句代码跟createdAt:Data | undefined
   }
+
+type Tag = {
+    id: string;
+    name: string;
+}
+type TagListModel = {
+    data: Tag[];
+    fetch: () => Tag[];
+    create: (name: string) => 'success' | 'duplicated';
+    save: () => void;
+    update: (id: string,name: string) => 'success'|'not found'|'duplicated';
+    remove: (id: string) => boolean;
+}
+
+interface Window{
+    tagList: Tag[];
+}
